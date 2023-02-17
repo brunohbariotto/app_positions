15b4a185db090ec0fccd54ee2fc1b0d1f0aabf91
BB <brunohbariotto@gmail.com>
2023-02-17 20:56:41 -0300

    t4

--- main.py
+++ main.py
@@ -10,34 +10,51 @@ import yfinance as yf
 import plotly.express as px
 import plotly.graph_objects as go
 from google.oauth2 import service_account
-from gsheetsdb import connect
-
+#from gsheetsdb import connect
+from gspread_pandas import Spread, Client
 
 st.write("New App Position!")
 st.write("Trying")
 
 
 
-# Create a connection object.
+# # Create a connection object.
+# credentials = service_account.Credentials.from_service_account_info(
+#     st.secrets["gcp_service_account"],
+#     scopes=[
+#         "https://www.googleapis.com/auth/spreadsheets",
+#     ],
+# )
+# conn = connect(credentials=credentials)
+
+# Create a google autentication object.
+scope=["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
+
 credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
+     st.secrets["gcp_service_account"],
+     scopes=scope)
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+client = Client(scope=scope, creds=credentials)
+spreadsheetname = "Position_Control"
 
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+spread = Spread(spreadsheetname, client = client)
 
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+st.write(spread.url)
+
+
+# conn = connect(credentials=credentials)
+
+# # Perform SQL query on the Google Sheet.
+# # Uses st.cache_data to only rerun when the query changes or after 10 min.
+# @st.cache_data(ttl=600)
+# def run_query(query):
+#     rows = conn.execute(query, headers=1)
+#     rows = rows.fetchall()
+#     return rows
+
+# sheet_url = st.secrets["private_gsheets_url"]
+# rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# # Print results.
+# for row in rows:
+#     st.write(f"{row.name} has a :{row.pet}:")
