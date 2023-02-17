6792d95073bfe0c4a77d1affaab05a81f664c8d0
BB <brunohbariotto@gmail.com>
2023-02-17 19:28:06 -0300

    inserindo main pela primeira vez

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 19:25:17 2023
+
+@author: Dell inspiron
+"""
+
+import streamlit as st
+import yfinance as yf
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+st.write("New App Position!")
