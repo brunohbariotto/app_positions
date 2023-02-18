b1a4ef432172d261ec3054d1a9f8a4b5d7d81454
BB <brunohbariotto@gmail.com>
2023-02-18 16:17:26 -0300

    Classe PlanGoogle criada

--- main.py
+++ main.py
@@ -10,45 +10,46 @@ import yfinance as yf
 import plotly.express as px
 import plotly.graph_objects as go
 from google.oauth2 import service_account
-#from gsheetsdb import connect
 from gspread_pandas import Spread, Client
 import pandas as pd
 from pages import Pages
+from plangoogle import PlanGoogle
 
 
 # Create a google autentication object.
-scope=["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
+# scope=["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
 
-credentials = service_account.Credentials.from_service_account_info(
-     st.secrets["gcp_service_account"],
-     scopes=scope)
+# credentials = service_account.Credentials.from_service_account_info(
+#      st.secrets["gcp_service_account"],
+#      scopes=scope)
 
-client = Client(scope=scope, creds=credentials)
-spreadsheetname = "Position_Control"
+# client = Client(scope=scope, creds=credentials)
+# spreadsheetname = "Position_Control"
 
-spread = Spread(spreadsheetname, client = client)
+# spread = Spread(spreadsheetname, client = client)
 
-st.write(spread.url)
+# st.write(spread.url)
 
-sh = client.open(spreadsheetname)
-worksheet_list = sh.worksheets()
+# sh = client.open(spreadsheetname)
+# worksheet_list = sh.worksheets()
 
-@st.cache(ttl=600)
-def worksheet_names():
-    sheet_names = []
-    for sheet in worksheet_list:
-        sheet_names.append(sheet.title)
-    return sheet_names
+# @st.cache(ttl=600)
+# def worksheet_names():
+#     sheet_names = []
+#     for sheet in worksheet_list:
+#         sheet_names.append(sheet.title)
+#     return sheet_names
 
-def load_spreadsheet(spreadsheetname):
-    worksheet = sh.worksheet(spreadsheetname)
-    df = pd.DataFrame(worksheet.get_all_records())
-    return df
+# def load_spreadsheet(spreadsheetname):
+#     worksheet = sh.worksheet(spreadsheetname)
+#     df = pd.DataFrame(worksheet.get_all_records())
+#     return df
 
-def update_spreadsheet(spreadsheetname, df):
-    spread.df_to_sheet(df, sheet=spreadsheetname, index=False)
-    st.info("Atualizado na Planilha !!!")
+# def update_spreadsheet(spreadsheetname, df):
+#     spread.df_to_sheet(df, sheet=spreadsheetname, index=False)
+#     st.info("Atualizado na Planilha !!!")
 
+gog = PlanGoogle()
 
 lista_menu = ['Controle de Posição', 'Mercado','Modelos', 'Relatórios']
 st.sidebar.subheader('Menu Principal')
@@ -58,11 +59,13 @@ escolha = st.sidebar.radio('Escolha a Opção: ', lista_menu)
 pg = Pages()
 
 if escolha == 'Controle de Posição':
-    df = load_spreadsheet('positions_BrunoBariotto')
+    df = gog.read_spreadsheet('positions_BrunoBariotto')
+    # df = load_spreadsheet('positions_BrunoBariotto')
     pg.posicao(df)
     
 if escolha == 'Mercado':
-    df = load_spreadsheet('positions_BrunoBariotto')
+    df = gog.read_spreadsheet('positions_BrunoBariotto')
+    # df = load_spreadsheet('positions_BrunoBariotto')
     pg.mercado(df)
 
 if escolha == 'Modelos':
---
+++ plangoogle.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 18 15:03:10 2023
+
+@author: Dell inspiron
+"""
+
+from google.oauth2 import service_account
+from gspread_pandas import Spread, Client
+import streamlit as st
+import pandas as pd
+
+class PlanGoogle:
+    def __init__(self):
+        self.scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
+        self.client = self.init_credentials()
+        
+    def init_credentials(self):
+        
+        credentials = service_account.Credentials.from_service_account_info(
+             st.secrets["gcp_service_account"],
+             scopes=self.scope)
+
+        client = Client(scope=self.scope, creds=credentials)
+        return client
+    
+    def read_spreadsheet(self, spreadsheetname):
+        
+        #spreadsheetname = "Position_Control"
+        spread = Spread(spreadsheetname, client = self.client)
+        st.write(spread.url)
+
+        sh = self.client.open(spreadsheetname)
+        worksheet = sh.worksheet(spreadsheetname)
+        df = pd.DataFrame(worksheet.get_all_records())
+        return df
+        #worksheet_list = sh.worksheets()
+
+    # @st.cache(ttl=600)
+    # def worksheet_names():
+    #     sheet_names = []
+    #     for sheet in worksheet_list:
+    #         sheet_names.append(sheet.title)
+    #     return sheet_names
+
+    # def load_spreadsheet(spreadsheetname):
+    #     worksheet = sh.worksheet(spreadsheetname)
+    #     df = pd.DataFrame(worksheet.get_all_records())
+    #     return df
+
+    # def update_spreadsheet(spreadsheetname, df):
+    #     spread.df_to_sheet(df, sheet=spreadsheetname, index=False)
+    #     st.info("Atualizado na Planilha !!!")
