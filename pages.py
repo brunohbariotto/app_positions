29a0add27edfeba6467c6620cbc74eb053d4c10e
BB <brunohbariotto@gmail.com>
2023-02-22 13:54:39 -0300

    atualizando df_oscilador e botão enviar plan19

--- pages.py
+++ pages.py
@@ -124,7 +124,7 @@ class Pages:
             st.write(df_send)
             st.write(df_osc)
             #df_osc.columns = 'pos_osc'
-            df_osc.rename(columns={df_osc.columns[0]:'pos_osc'}, inplace=True)
+            df_osc.rename(columns={df_osc.columns:'pos_osc'}, inplace=True)
             st.write(df_osc)
             df_send.update(df_osc)
             st.write('Depois')
