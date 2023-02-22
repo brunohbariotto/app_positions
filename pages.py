3c18eff22f529740b9633202c5e1bba6d3ae4fb0
BB <brunohbariotto@gmail.com>
2023-02-22 13:43:45 -0300

    atualizando df_oscilador e botão enviar plan14

--- pages.py
+++ pages.py
@@ -122,8 +122,8 @@ class Pages:
             df_send = df.copy().set_index('Acao')
             st.write('Antes')
             st.write(df_send)
-            st.write(df_osc.iloc[:,0])
-            df_send['pos_atual'] = df_osc.iloc[:,0]
+            st.write(df_osc)
+            df_send['pos_atual'] = df_osc
             st.write('Depois')
             st.write(df_send.reset_index())
             
