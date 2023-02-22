356894f29d3eeb48592e4f4d474d30726e8174e4
BB <brunohbariotto@gmail.com>
2023-02-22 10:14:50 -0300

    Inserindo Retornos em Mercado e Modelos6

--- pages.py
+++ pages.py
@@ -90,7 +90,7 @@ class Pages:
         df_prices = m.download_prices(list(df.Acao), per_data, anos_cotacoes, datas_inicio, datas_fim)
         st.write(df_prices)
         df_returns = m.returns(df_prices)
-        st.write(df_prices)
+        st.write(df_returns)
         
     
     # modelos
@@ -106,7 +106,7 @@ class Pages:
         df_prices = m.download_prices(list(df.Acao), per_data, anos_cotacoes, datas_inicio, datas_fim)
         st.write(df_prices)
         df_returns = m.returns(df_prices)
-        st.write(df_prices)
+        st.write(df_returns)
         
     
     #relatorio
