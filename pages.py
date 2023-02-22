9e352df7147458d8d8eee80dff6ea057bbf79112
BB <brunohbariotto@gmail.com>
2023-02-22 14:20:59 -0300

    atualizando df_oscilador e botão enviar plan26

--- pages.py
+++ pages.py
@@ -120,18 +120,23 @@ class Pages:
         if modelo == 'Oscilador':
             df_osc = m.oscilador(df_prices.loc[:,mult_simb]).copy()
             df_send = df.copy().set_index('Acao')
-            st.write('Antes')
-            st.write(df_send)
-            st.write(df_osc)
             
-            st.write('Depois')
-            df_send.loc['pos_osc'] = 100
-            st.write(df_send.reset_index())
-            #df_osc.columns = 'pos_osc'
-            # df_osc.rename(columns={df_osc.columns[0]:'pos_osc'}, inplace=True)
-            # df_send.update(df_osc)
+            st.write(list(df_osc))
+            st.write(df_send)
+            
+            
+            # st.write('Antes')
+            # st.write(df_send)
+            # st.write(df_osc)
+            
             # st.write('Depois')
+            # df_send.loc['pos_osc'] = 100
             # st.write(df_send.reset_index())
+            # #df_osc.columns = 'pos_osc'
+            # # df_osc.rename(columns={df_osc.columns[0]:'pos_osc'}, inplace=True)
+            # # df_send.update(df_osc)
+            # # st.write('Depois')
+            # # st.write(df_send.reset_index())
             
             if st.button('Atualizar Oscilador?'):
                 st.write('Enviando...')
