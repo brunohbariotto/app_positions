16eda422810d6cdad0cb846a978b4b6010fa24e0
BB <brunohbariotto@gmail.com>
2023-03-24 16:02:14 -0300

    ajustando dolar em btc e spx5

--- pages.py
+++ pages.py
@@ -50,15 +50,20 @@ class Pages:
         st.plotly_chart(fig)
         
         markowitz_usd = 29979.47076*df[df['Acao'] == 'USDBRL=X']['pos_osc']/100
+        st.write('USD-BRL MarkxOsc')
         st.write(markowitz_usd)
         
         markowitz_btc = markowitz_usd*(1.62/(1.62+15.56))/1.5
         markowitz_spx = markowitz_usd*(15.56/(1.62+15.56))/1.5
         
-        st.write(markowitz_btc)
-        st.write(markowitz_spx)
+        st.write('BTC MarkxOsc')
+        st.write(markowitz_btc.values)
+        st.write('SPX MarkxOsc')
+        st.write(markowitz_spx.values)
         
         df['pos_oscxmark'] = df.pos_osc*df.pos_markw/100
+        
+        st.write(df_prices)
         
         st.write(df)
         
