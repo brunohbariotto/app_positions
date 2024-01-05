dcf7f2df04d2cbc0e75d213390a511b327a62ceb
BB <brunohbariotto@gmail.com>
2024-01-05 14:21:39 -0300

    fundamentos acoes

--- pages.py
+++ pages.py
@@ -726,7 +726,7 @@ class Pages:
             
             st.write('Atualizar: acessar https://statusinvest.com.br/acoes/busca-avancada , clicar em buscar e fazer download do csv')
             import os
-            import openpyxl
+            #import openpyxl
             st.write(os.getcwd())
             st.write('Salvar a planilha em C:/Users/Dell inspiron/app_positions')
             
