26067e85e585fb5855f543a1981aa2d24bc4a6e0
BB <brunohbariotto@gmail.com>
2024-01-05 14:46:03 -0300

    fundamentos acoes7

--- pages.py
+++ pages.py
@@ -732,7 +732,7 @@ class Pages:
             
             ind_df = pd.read_csv('statusinvest-busca-avancada.csv', sep=';')
             
-            setores_df = pd.read_csv('setores.csv', engine='python')
+            setores_df = pd.read_csv('setores.csv', engine='python', encoding='utf-8')
             st.write(setores_df)
             
             ind_df_final = pd.concat([ind_df,setores_df], axis=1, join='inner')
