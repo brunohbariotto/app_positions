423ca38f9a0f0351d0fcba1cdbb2f4dde44a50d7
BB <brunohbariotto@gmail.com>
2024-01-12 14:31:50 -0300

    Filtro empresas semelhantes4

--- pages.py
+++ pages.py
@@ -771,13 +771,14 @@ class Pages:
                 st.write('Filtro 1) Margem EBIT > 0 para garantir que Div Liq/Ebit menor melhor')
                 
                 st.subheader('Indicadores de Valuation: P/EBIT e DY')
-                st.write(ind_df_final[ind_df_final['SEGMENTO'] == segmento][['TICKER','P/EBIT','DY','P/L','P/VP','P/ATIVOS']])
+                val_df = ind_df_final[ind_df_final['SEGMENTO'] == segmento][['TICKER','P/EBIT','DY','P/L','P/VP','P/ATIVOS']]
+                st.write(val_df[val_df >= 0])
                 
                 st.subheader('Indicadores de Rentabilidade: Margem EBIT e ROIC')
                 st.write(ind_df_final[ind_df_final['SEGMENTO'] == segmento][['MARGEM EBIT','ROIC','MARGEM BRUTA','MARG. LIQUIDA','ROE','ROA']])
                 
                 st.subheader('Indicadores de Endividamento: DIVIDA LIQUIDA / EBIT, LIQ. CORRENTE')
-                st.write(ind_df_final[ind_df_final['SEGMENTO'] == segmento][['TICKER','DIVIDA LIQUIDA/EBIT','LIQ.CORRENTE','LIQUIDEZ MEDIA DIARIA']])
+                st.write(ind_df_final[ind_df_final['SEGMENTO'] == segmento][['TICKER','DIVIDA LIQUIDA/EBIT','LIQ.CORRENTE',' LIQUIDEZ MEDIA DIARIA']])
 
                     
             
